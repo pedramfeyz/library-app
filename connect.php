2f08a367a8c8e0de074f0cee4d65ac81b4a0bb66
pedramfeyz <pedram_feyz@yahoo.com>
2016-07-13 12:47:31 +1000

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
 
-mysql_connect('localhost','pedramlib','pedramlib123') or die('we can\'t connect to servser:'.mysql_error());
+mysql_connect('localhost','username','pass') or die('we can\'t connect to servser:'.mysql_error());
 mysql_select_db('libbookstore') or die('we can\'t connect to database:'.mysql_error());
 ?>
